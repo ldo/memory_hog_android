3a5232111c8e85b5bed3acd086c3b0327dfd6e42
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2011-08-03 05:02:42 +0000

    add instructions and Apache licence

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,19 @@ package nz.gen.geek_central.MemoryHog;
     Then you discover that everything prior to that has also been
     killed, including the launcher. Bit of a sledgehammer, eh wot...
 
-    Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+    Copyright 2011 by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you
+    may not use this file except in compliance with the License. You may
+    obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+    implied. See the License for the specific language governing
+    permissions and limitations under the License.
 */
 
 public class Main extends android.app.Activity
