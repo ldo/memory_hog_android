3d9193024ae488b14e8e3ea67fcf28b9d4362231
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2013-06-19 11:25:09 +0000

    use more general implementation of save/restore instance state

--- src/DigitSpinner.java
+++ src/DigitSpinner.java
@@ -2,7 +2,7 @@ package nz.gen.geek_central.MemoryHog;
 /*
     Custom spinner showing digits 0 .. 9.
 
-    Copyright 2011 by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+    Copyright 2011, 2013 by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
 
     Licensed under the Apache License, Version 2.0 (the "License"); you
     may not use this file except in compliance with the License. You may
@@ -16,6 +16,8 @@ package nz.gen.geek_central.MemoryHog;
     implied. See the License for the specific language governing
     permissions and limitations under the License.
 */
+
+import nz.gen.geek_central.android.useful.BundledSavedState;
 
 public class DigitSpinner extends android.widget.RelativeLayout
   {
@@ -156,90 +158,15 @@ public class DigitSpinner extends android.widget.RelativeLayout
     allows me to transparently restore widget setting if system
     needs to kill me while I'm in the background, or on an orientation
     change while I'm in the foreground.
-
-    Notes: View.onSaveInstanceState returns AbsSavedState.EMPTY_STATE,
-    and View.onRestoreInstanceState expects to be passed this. Also,
-    both superclass methods MUST be called in my overrides (the docs
-    don't make this clear).
-
-    This class subclasses RelativeLayout, which subclasses ViewGroup,
-    neither of which overrides the above View methods. So those points
-    still hold.
   */
-    protected static class SavedDigitSpinnerState extends android.view.AbsSavedState
-      {
-
-        public static android.os.Parcelable.Creator<SavedDigitSpinnerState> CREATOR =
-            new android.os.Parcelable.Creator<SavedDigitSpinnerState>()
-              {
-                public SavedDigitSpinnerState createFromParcel
-                  (
-                    android.os.Parcel SavedState
-                  )
-                  {
-                    final android.view.AbsSavedState SuperState =
-                        android.view.AbsSavedState.CREATOR.createFromParcel(SavedState);
-                    final android.os.Bundle MyState = SavedState.readBundle();
-                    return
-                        new SavedDigitSpinnerState
-                          (
-                            SuperState,
-                            MyState.getInt("CurDigit", 0)
-                          );
-                  } /*createFromParcel*/
-
-                public SavedDigitSpinnerState[] newArray
-                  (
-                    int NrElts
-                  )
-                  {
-                    return
-                        new SavedDigitSpinnerState[NrElts];
-                  } /*newArray*/
-              } /*Parcelable.Creator*/;
-
-        public final android.os.Parcelable SuperState;
-      /* state that I'm actually interested in saving/restoring: */
-        public final int CurDigit;
-
-        public SavedDigitSpinnerState
-          (
-            android.os.Parcelable SuperState,
-            int CurDigit
-          )
-          {
-            super(SuperState);
-            this.SuperState = SuperState;
-            this.CurDigit = CurDigit;
-          } /*SavedDigitSpinnerState*/
-
-        public void writeToParcel
-          (
-            android.os.Parcel SavedState,
-            int Flags
-          )
-          {
-            super.writeToParcel(SavedState, Flags);
-          /* put my state in a Bundle, where each item is associated with a
-            keyword name (unlike the Parcel itself, where items are identified
-            by order). I think this makes things easier to understand, and extend
-            in future. */
-            final android.os.Bundle MyState = new android.os.Bundle();
-            MyState.putInt("CurDigit", CurDigit);
-            SavedState.writeBundle(MyState);
-          } /*writeToParcel*/
-
-      } /*SavedDigitSpinnerState*/
 
     @Override
     public android.os.Parcelable onSaveInstanceState()
       {
+        final android.os.Bundle MyState = new android.os.Bundle();
+        MyState.putInt("CurDigit", CurDigit);
         return
-            new SavedDigitSpinnerState
-              (
-                super.onSaveInstanceState(),
-                CurDigit
-              );
+            new BundledSavedState(super.onSaveInstanceState(), MyState);
       } /*onSaveInstanceState*/
 
     @Override
@@ -248,9 +175,9 @@ public class DigitSpinner extends android.widget.RelativeLayout
         android.os.Parcelable SavedState
       )
       {
-        final SavedDigitSpinnerState MyState = (SavedDigitSpinnerState)SavedState;
-        super.onRestoreInstanceState(MyState.SuperState);
-        SetDigit(MyState.CurDigit);
+        super.onRestoreInstanceState(((BundledSavedState)SavedState).SuperState);
+        final android.os.Bundle MyState = ((BundledSavedState)SavedState).MyState;
+        SetDigit(MyState.getInt("CurDigit", 0));
       } /*onRestoreInstanceState*/
 
   } /*DigitSpinner*/
